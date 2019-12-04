574a60e819a30ecdc65b0a602cbbd567448c6520
Winston Pemberton <winstonpemberton@gmail.com>
2019-12-04 23:33:20 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -11,7 +11,7 @@ class Triangle
 
   def kind
 
-    if side_1 == side_2 && side_1 == side_3
+    if side_1 == side_2 && side_1 == side_3 && side_2 == side_3
       :equilateral
     elsif side_1 == side_2 || side_1 == side_3 || side_3 = side_2
       :isosceles
