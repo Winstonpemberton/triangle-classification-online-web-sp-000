b5b8acf2aca8d584e5ca094a9b9c3a6688ddf3e4
Winston Pemberton <winstonpemberton@gmail.com>
2019-12-04 23:21:50 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,5 +1,32 @@
 class Triangle
   # write code here
 
-  attr_accessor :side1, :side2, :side3
+  attr_accessor :side_1, :side_2, :side_3
+
+  def initialize(side_1,side_2,side_3)
+    @side_1 = side_1
+    @side_2 = side_2
+    @side_3 = side_3
+  end
+
+  def kind
+
+    if side_1 == side_2 && side_1 == side_3
+      :equilateral
+    elsif side_1 == side_2 || side_1 == side_3 || side_3 = side_2
+      :isosceles
+    elsif side_1 != side_2 && side_1 != side3 && side_2 != side_3
+      :scalene
+    else
+      if side_1 <= 0 || side_2 <=0 || side_3 <= 0
+        raise
+        
+      
+  end
+
+  class TriangleError < StandardError
+    def message 
+      puts "Incorrect side measurement passed"
+    end
+  end
 end
