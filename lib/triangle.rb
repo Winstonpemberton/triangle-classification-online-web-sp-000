50b2860e77b5b774f698f7ab7288e4c837dadda2
Winston Pemberton <winstonpemberton@gmail.com>
2019-12-04 23:51:23 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -27,6 +27,7 @@ class Triangle
         # rescue TriangleError => error
         #   puts error.message
         # end
+      elsif side_1 + side_2 <= side_3 || side_1 + side_3 <= side_2 || side_2 + side_3 <= side_1
       end
     end
   end
