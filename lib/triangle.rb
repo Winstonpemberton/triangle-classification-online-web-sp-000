a463703fab8189a485a1468ad6880cb80d9a575c
Winston Pemberton <winstonpemberton@gmail.com>
2019-12-04 23:34:42 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -17,6 +17,7 @@ class Triangle
       :isosceles
     elsif side_1 != side_2 && side_1 != side3 && side_2 != side_3
       :scalene
+      binding.pry
     else
       if side_1 <= 0 || side_2 <=0 || side_3 <= 0
         begin
