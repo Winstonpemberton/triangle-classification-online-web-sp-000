ba765680e22cd9177faeab9f5aacdda8ec3816f1
Winston Pemberton <winstonpemberton@gmail.com>
2019-12-04 23:29:42 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -16,6 +16,7 @@ class Triangle
         raise TriangleError
       rescue TriangleError => error
         puts error.message
+      end
     else
       if side_1 == side_2 && side_1 == side_3
         :equilateral
