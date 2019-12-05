5b2887484676036f0f4876963aeb93d38d4d87fa
Winston Pemberton <winstonpemberton@gmail.com>
2019-12-05 00:02:05 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -18,7 +18,7 @@ class Triangle
       # rescue TriangleError => error
       #   puts error.message
       # end
-    end
+
     elsif side_1 + side_2 <= side_3 || side_1 + side_3 <= side_2 || side_2 + side_3 <= side_1
       raise TriangleError
 
@@ -31,6 +31,7 @@ class Triangle
       elsif side_1 != side_2 && side_1 != side_3 && side_2 != side_3
         :scalene
       #  binding.pry
+    end
 
     end
 
