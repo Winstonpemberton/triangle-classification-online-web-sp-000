3fee49a123fbf08780d25f3f05633867f3d32e01
Winston Pemberton <winstonpemberton@gmail.com>
2019-12-05 00:01:43 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -12,24 +12,25 @@ class Triangle
 
   def kind
 
-    if side_1 == side_2 && side_1 == side_3 && side_2 == side_3
-      :equilateral
-    elsif side_1 == side_2 || side_1 == side_3 || side_2 == side_3
-      :isosceles
-      #binding.pry
-    elsif side_1 != side_2 && side_1 != side_3 && side_2 != side_3
-      :scalene
-    #  binding.pry
-    else
-      if side_1 <= 0 || side_2 <=0 || side_3 <= 0
-        #begin
-          raise TriangleError
-        # rescue TriangleError => error
-        #   puts error.message
-        # end
-      elsif side_1 + side_2 <= side_3 || side_1 + side_3 <= side_2 || side_2 + side_3 <= side_1
+    if side_1 <= 0 || side_2 <=0 || side_3 <= 0
+      #begin
         raise TriangleError
-      end
+      # rescue TriangleError => error
+      #   puts error.message
+      # end
+    elsif side_1 + side_2 <= side_3 || side_1 + side_3 <= side_2 || side_2 + side_3 <= side_1
+      raise TriangleError
+
+    else
+      if side_1 == side_2 && side_1 == side_3 && side_2 == side_3
+        :equilateral
+      elsif side_1 == side_2 || side_1 == side_3 || side_2 == side_3
+        :isosceles
+        #binding.pry
+      elsif side_1 != side_2 && side_1 != side_3 && side_2 != side_3
+        :scalene
+      #  binding.pry
+
     end
 
   end
